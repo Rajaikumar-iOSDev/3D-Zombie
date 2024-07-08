17646c5f9b444d9763e2e8ed34065d4d4b8a2085
Rajai kumar <writetorajai@outlook.com>
2024-07-09 01:10:43 +0530

    WIP
    - Explosion sprite and audio added

--- main.go
+++ main.go
@@ -19,6 +19,35 @@ func main() {
     camera.Up = rl.NewVector3(0.0, 1.0, 0.0)       // Camera up vector (rotation towards target)
     camera.Fovy = 60.0                             // Camera field-of-view Y
     camera.Projection = rl.CameraPerspective             // Camera projection type
+      // Initialize audio device
+      rl.InitAudioDevice()
+      defer rl.CloseAudioDevice()
+  
+      // Load explosion sound
+      fxBoom := rl.LoadSound("resources/boom.wav")
+      defer rl.UnloadSound(fxBoom)
+  
+      // Load explosion texture
+      explosion := rl.LoadTexture("resources/explosion.png")
+      defer rl.UnloadTexture(explosion)
+  
+      // Init variables for animation
+      const NUM_FRAMES_PER_LINE = 5 // Example value, replace with actual number
+      const NUM_LINES = 5           // Example value, replace with actual number
+  
+      frameWidth := float32(explosion.Width) / float32(NUM_FRAMES_PER_LINE)   // Sprite one frame rectangle width
+      frameHeight := float32(explosion.Height) / float32(NUM_LINES)           // Sprite one frame rectangle height
+      currentFrame := 0
+      currentLine := 0
+
+       // Define frame rectangle and position
+    frameRec := rl.NewRectangle(0, 0, frameWidth, frameHeight)
+    position := rl.NewVector2(0.0, 0.0)
+
+    // Animation control variables
+    active := false
+    framesCounter := 0
+  
 
     var heights [maxColumns]float32
     var positions [maxColumns]rl.Vector3
@@ -29,9 +58,9 @@ func main() {
         positions[i] = rl.NewVector3(float32(rl.GetRandomValue(-15, 15)), heights[i]/2.0, float32(rl.GetRandomValue(-15, 15)))
         colors[i] = rl.NewColor(uint8(rl.GetRandomValue(20, 255)), uint8(rl.GetRandomValue(10, 55)), 30, 255)
     }
-    //Load handgun model
-    model := rl.LoadModel("plane.obj");                  // Load model
-    texture := rl.LoadTexture("plane_diffuse.png");  // Load model texture
+
+    model := rl.LoadModel("resources/plane.obj");                  // Load model
+    texture := rl.LoadTexture("resources/plane_diffuse.png");  // Load model texture
     mat1 := model.GetMaterials()
     mat1[0].GetMap(rl.MapDiffuse).Texture = texture // Set model diffuse texture
 
@@ -42,6 +71,41 @@ func main() {
     // Main game loop
     for !rl.WindowShouldClose() {
         rl.UpdateCamera(&camera, rl.CameraFirstPerson) // Update camera
+            // Check for mouse button pressed and activate explosion (if not active)
+            if rl.IsKeyPressed(rl.KeySpace) && !active {
+                active = true
+    
+                 position.X = float32(screenWidth) / 2.0
+                 position.Y = float32(screenHeight) / 2.0
+                 position.X -= frameWidth / 2.0
+                 position.Y -= frameHeight / 2.0 - 90.0
+    
+                rl.PlaySound(fxBoom)
+            }
+    
+            // Compute explosion animation frames
+            if active {
+                framesCounter++
+    
+                if framesCounter > 2 {
+                    currentFrame++
+    
+                    if currentFrame >= NUM_FRAMES_PER_LINE {
+                        currentFrame = 0
+                        currentLine++
+    
+                        if currentLine >= NUM_LINES {
+                            currentLine = 0
+                            active = false
+                        }
+                    }
+    
+                    framesCounter = 0
+                }
+            }
+    
+            frameRec.X = frameWidth * float32(currentFrame)
+            frameRec.Y = frameHeight * float32(currentLine)
 
         rl.BeginDrawing()
 
@@ -50,15 +114,16 @@ func main() {
         rl.BeginMode3D(camera)
 		rl.DrawPlane(rl.NewVector3(0.0, 0.0, 0.0), rl.NewVector2(102.0, 102.0), rl.Brown)
         for i := 0; i < maxColumns; i++ {
-            // rl.NewBoundingBox(rl.NewVector3(positions[i].X-1.0, positions[i].Y-heights[i]/2.0, positions[i].Z-1.0), rl.NewVector3(positions[i].X+1.0, positions[i].Y+heights[i]/2.0, positions[i].Z+1.0))
             rl.DrawCube(positions[i], 2.0, heights[i], 2.0, colors[i])
             rl.DrawCubeWires(positions[i], 2.0, heights[i], 2.0, rl.Gray)
         }
         rl.DrawModel(model,camera.Target,0.03,rl.White)
-        // rl.DrawGrid(10, 10.0)
-        // rl.DrawModelWires(handGun,camera.Target,1.0,rl.Blue)
 
+        
         rl.EndMode3D()
+        if active {
+              rl.DrawTextureRec(explosion, frameRec, position, rl.White)
+        }
 
         rl.EndDrawing()
     }
